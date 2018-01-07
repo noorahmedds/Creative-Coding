a155edc008eb5465eec377970024860e29ef1e52
Noor Ahmed <noorahmedds@outlook.com>
2018-01-07 21:45:36 +0500

    ArcPaths
    created arc paths for stars instead of warp field. The depth effect is much more satisfying now. :p

--- stargazing/sketch.js
+++ stargazing/sketch.js
@@ -1,3 +1,4 @@
+var starSize = 2.5;
 
 function Star(sp, foo){
 	//spawn code
@@ -7,16 +8,19 @@ function Star(sp, foo){
 	this.sz = 0;
 	this.speed = sp;
 	this.flicker = foo;
+	
+	this.arcLength = random(100, 700);
 
 	this.update = function(){
 		//updating the z value to transform the star
 		this.z -= this.speed;
-		this.sz = map(this.z, 0, width, 5, 0);
+		this.sz = map(this.z, 0, width, starSize, 0);
 		
 		//resetting the star
 		if(this.z < 1){
 			this.x = random(-width, width);
 			this.y = random(-height, height);
+			this.arcLength = random(100, 700);
 			
 			//set the star back to farthest point
 			this.z = width;
@@ -34,8 +38,8 @@ function Star(sp, foo){
 		
 		
 		//this code  the star
-		var sx = map((this.x/this.z), 0, 1, 0, width);
-		var sy = map((this.y/this.z), 0, 1, 0, height);
+		var sx = map(abs(this.x/this.z), 0, 1, width, 0);
+		var sy = 0 - Math.sqrt(Math.pow(this.arcLength, 2) - Math.pow((-1*sx), 2));
 		ellipse(sx, sy, this.sz, this.sz);
 	}
 }
@@ -43,8 +47,8 @@ function Star(sp, foo){
 
 
 var stars = [];
-var starAmount = 100;
-var slowerStarAmount = 400;
+var starAmount = 300;
+var slowerStarAmount = 600;
 
 function setup() {
 	console.log(map(-0.5, 0,1, 0, 600));
@@ -52,12 +56,11 @@ function setup() {
   createCanvas(600, 600);
 	// frameRate(10);
 	for(var i =0; i < starAmount; i++){
-		stars.push(new Star(5, false));
-		stars.push(new Star(15, false));
+		stars.push(new Star(0.05, false));
 	}
 	
 	for(var j =0; j < slowerStarAmount; j++){
-		stars.push(new Star(1, true));
+		stars.push(new Star(0.01, true));
 	}
 } 
 
@@ -67,7 +70,7 @@ function draw() {
 	// background(red, green, blue);
 	background(0);
 	
-	translate(width / 2, height / 2);
+	translate(width / 2, height+70);
   for(var i =0; i < starAmount+slowerStarAmount; i++){
 		stars[i].update();
 		stars[i].draw();
